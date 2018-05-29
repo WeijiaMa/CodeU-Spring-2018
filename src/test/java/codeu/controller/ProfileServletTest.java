bcf40acd7fc55a8e62bead850047f6b5c0397311
konceq <clifford62442>
2018-05-29 12:15:41 -0400

    Finished test cases for prototype. Corrected minor syntax issues from last pull request.

--- src/test/java/codeu/controller/ProfileServletTest.java
+++ src/test/java/codeu/controller/ProfileServletTest.java
@@ -8,14 +8,16 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class ProfileServletTest {
-    /*
+
     private ProfileServlet profileServlet;
     private HttpServletRequest mockRequest;
+    private HttpSession mockSession;
     private HttpServletResponse mockResponse;
     private RequestDispatcher mockRequestDispatcher;
 
@@ -24,6 +26,9 @@ public class ProfileServletTest {
     public void setup() {
         profileServlet = new ProfileServlet();
         mockRequest = Mockito.mock(HttpServletRequest.class);
+        mockSession = Mockito.mock(HttpSession.class);
+        Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+
         mockResponse = Mockito.mock(HttpServletResponse.class);
         mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
         Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/profile.jsp"))
@@ -32,9 +37,12 @@ public class ProfileServletTest {
 
     @Test
     public void testDoGet() throws IOException, ServletException {
+        Mockito.when(mockRequest.getRequestURI()).thenReturn("/users/test_profile");
         profileServlet.doGet(mockRequest, mockResponse);
+        String fakeUser = mockRequest.getRequestURI().substring("/users/".length());
+        Mockito.verify(mockRequest).setAttribute("user", fakeUser);
         Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
     }
-    */
+
 
 }
